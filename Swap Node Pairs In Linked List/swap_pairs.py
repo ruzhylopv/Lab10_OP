a93a3f4b1a8863acc6f58a2c7782631d9efac5b5
Pavlo Ruzhylo <ruzhylo.pn@ucu.edu.ua>
2025-04-02 17:52:55 +0300

    swap pairs tests passed

---
+++ Swap Node Pairs In Linked List/swap_pairs.py
@@ -0,0 +1,42 @@
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+    def __repr__(self):
+        return f'{self.data} -> {self.next}'
+    def __str__(self):
+        return stringify(self)
+
+def stringify(node):
+    s = ''
+    while node is not None:
+        value = node.data
+        node = node.next
+        s += str(value) + ' -> '
+    return s + 'None'
+
+def build(array):
+    head = Node(array[0])
+    probe = head
+    for i in range(1, len(array)):
+        probe.next = Node(array[i])
+        probe = probe.next
+    return head
+
+
+def swap_pairs(head):
+    if head.next is None:
+        return head
+    probe = head.next
+    prev = head
+
+    prev.next = swap_pairs(probe.next)
+    probe.next = prev
+    return probe
+
+
+
+tst = build([1, 2, 3, 4, 5])
+print('Original list')
+print(swap_pairs(tst))
